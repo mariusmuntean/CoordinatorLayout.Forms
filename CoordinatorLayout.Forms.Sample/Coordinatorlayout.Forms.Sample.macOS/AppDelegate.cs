1b44476792c0e5998924dd0bad4d7cbd00fa4174
Marius Muntean <marius.muntean@live.com>
2020-05-02 14:35:03 +0200

    Added a macOS hosting project

---
+++ CoordinatorLayout.Forms.Sample/Coordinatorlayout.Forms.Sample.macOS/AppDelegate.cs
@@ -0,0 +1,35 @@
+﻿using AppKit;
+using CoordinatorLayout.XamarinForms.Sample;
+using Foundation;
+using Xamarin.Forms.Platform.MacOS;
+
+namespace Coordinatorlayout.Forms.Sample.macOS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : FormsApplicationDelegate
+    {
+        public AppDelegate()
+        {
+            var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+
+            var rect = new CoreGraphics.CGRect(200, 1000, 1024, 768);
+            MainWindow = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+            MainWindow.Title = "Xamarin.Forms on Mac!"; // choose your own Title here
+            MainWindow.TitleVisibility = NSWindowTitleVisibility.Hidden;
+        }
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+            base.DidFinishLaunching(notification);
+        }
+
+        public override NSWindow MainWindow { get; }
+
+        public override void WillTerminate(NSNotification notification)
+        {
+            // Insert code here to tear down your application
+        }
+    }
+}
